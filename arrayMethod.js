2c16952440b4860aaee9e883d53dd99da0523ff3
Ty1759 <oyelekanoyebisi@gmail.com>
2024-11-21 14:20:17 +0100

    Advance Javascript

---
+++ arrayMethod.js
@@ -0,0 +1,33 @@
+const fruits = ["Apple", "Orange", "Banana", "Coconut"];
+console.log(fruits);
+
+fruits.push("Cherry") //add to the fruits at the end
+console.log(fruits);
+
+fruits.unshift("Mango") // add to the array of fruits at the beginning.
+console.log(fruits);
+
+fruits.pop(); // removes the last element in the array
+console.log(fruits);
+
+fruits.shift(); // removes the first element in the array
+console.log(fruits)
+
+fruits.splice(1, 1); // this method is used to remove from a certain area in the array.
+console.log(fruits)
+
+fruits[1] = "blueberry" // this method is used to replace a string in the array
+console.log(fruits)
+
+fruits.splice(1, 0, "starwberry",  "Pineapple", "Tangerine", "Watermelon"); // removes element and if necessary add another in their place
+console.log(fruits);
+
+for (let i = 0; i < fruits.length; i++) {
+    console.log( fruits[i]);
+}
+
+let singleFruit = fruits.map(fruit => console.log(fruit))
+
+let upperCaseFruits = fruits.map(fruit => fruit.toUpperCase());
+console.log(upperCaseFruits)
+
